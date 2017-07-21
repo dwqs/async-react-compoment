f170ca68e75478d1e4f73489bf7855f449cb2f71
dwqs <461147874@qq.com>
2017-07-21 19:25:45 +0800

    [misc]update

---
+++ src/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export interface AsyncState {
+    Component: any
+}
+
+function getAsyncComponent<T>(promise: Promise<T>): any {
+	return class AsyncComponent extends React.Component<{}, AsyncState>{
+        public state: AsyncState;
+        
+		constructor(props: {}) {
+            super(props);
+            this.state = {
+                Component: null,
+            };
+        }
+
+		public componentWillMount() {
+            if (!this.state.Component) {
+                promise.then((mod: any) => {
+                    this.setState({
+                        Component: mod.default ? mod.default : mod,
+                    });
+                });
+            }
+        }
+		
+        public render() {
+            const { Component } = this.state;
+            if (Component) {
+                return (
+                     <Component {...this.props} />
+                );
+            }
+            // Failed to resolve async component
+            return null;
+        }
+	}
+}
+
+export default {
+	getAsyncComponent,
+};
