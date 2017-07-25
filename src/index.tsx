63c91da643cb32fbfe2aeb6fe68094afdaff7a1c
Pomy <dwqs@users.noreply.github.com>
2017-07-25 20:25:59 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,35 +6,35 @@ export interface AsyncState {
 
 function getAsyncComponent<T>(promise: () => Promise<T>): any {
 	return class AsyncComponent extends React.Component<{}, AsyncState>{
-        public state: AsyncState;
-        
+		public state: AsyncState;
+
 		constructor(props: {}) {
-            super(props);
-            this.state = {
-                Component: null,
-            };
-        }
+		    super(props);
+		    this.state = {
+			Component: null,
+		    };
+		}
 
 		public componentWillMount() {
-            if (!this.state.Component) {
-                promise().then((mod: any) => {
-                    this.setState({
-                        Component: mod.default ? mod.default : mod,
-                    });
-                });
-            }
-        }
-		
-        public render() {
-            const { Component } = this.state;
-            if (Component) {
-                return (
-                     <Component {...this.props} />
-                );
-            }
-            // Failed to resolve async component
-            return null;
-        }
+		    if (!this.state.Component) {
+			promise().then((mod: any) => {
+			    this.setState({
+				Component: mod.default ? mod.default : mod,
+			    });
+			});
+		    }
+		}
+
+		public render() {
+		    const { Component } = this.state;
+		    if (Component) {
+			return (
+			     <Component {...this.props} />
+			);
+		    }
+		    // Failed to resolve async component
+		    return null;
+		}
 	}
 }
 
