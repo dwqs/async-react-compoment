f99e8160f002a93f11e4d273a7bdfdfb496e1da4
dwqs <461147874@qq.com>
2017-07-25 20:29:00 +0800

    [misc]formate

--- src/index.tsx
+++ src/index.tsx
@@ -5,37 +5,37 @@ export interface AsyncState {
 }
 
 function getAsyncComponent<T>(promise: () => Promise<T>): any {
-     return class AsyncComponent extends React.Component<{}, AsyncState>{
-	public state: AsyncState;
+    return class AsyncComponent extends React.Component<{}, AsyncState>{
+        public state: AsyncState;
 
-	constructor(props: {}) {
-	   super(props);
-	   this.state = {
-		Component: null,
-	    };
-	}
+        constructor(props: {}) {
+            super(props);
+            this.state = {
+                Component: null,
+            };
+        }
 
-	public componentWillMount() {
-	    if (!this.state.Component) {
-		promise().then((mod: any) => {
-		    this.setState({
-			Component: mod.default ? mod.default : mod,
-		    });
-		});
-	    }
-	}
+        public componentWillMount() {
+            if (!this.state.Component) {
+                promise().then((mod: any) => {
+                    this.setState({
+                        Component: mod.default ? mod.default : mod,
+                    });
+                });
+            }
+        }
 
-	public render() {
-	    const { Component } = this.state;
-	    if (Component) {
-		return (
-		     <Component {...this.props} />
-		);
-	    }
-	    // Failed to resolve async component
-	    return null;
-	}
-     }
+        public render() {
+            const { Component } = this.state;
+            if (Component) {
+                return (
+                    <Component {...this.props} />
+                );
+            }
+            // Failed to resolve async component
+            return null;
+        }
+    }
 }
 
 const ReactAsyncComponent = {
